63db12985742c07117d06e904b6135425398316f
Himanshi Rawat <himanshirawat604@gmail.com>
2023-08-04 11:35:11 +0530

    changes

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ const dotenv = require("dotenv");
 
 
 dotenv.config();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials','true');
+
+  if (req.method === 'OPTIONS'){
+    return res.status(204).end();
+  }
+
+  next();
+});
+
+
+
 const hotelDataAddedToDBRouter = require("./routes/dataimport.router");
 const categoryDataAddedToDBRouter = require("./routes/categoryimport.router");
 
